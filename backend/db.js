0c6f6b8f5579023d816bd17cb5fbf833bdb38595
sameer kumar sahu <sameer66sahu@gmail.com>
2025-01-05 16:16:14 +0530

    update

--- backend/db.js
+++ backend/db.js
@@ -33,9 +33,24 @@ const userSchema = new mongoose.Schema({
 });
 
 //Create a model for the Schema
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+
+const Account = mongoose.model('Account', accountSchema);
 const User = mongoose.model('User', userSchema);
 
 
 module.exports={
-    User
+    User,
+    Account
 };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
 const router = express.Router();
+
+
+//to handle the all user req 
+const userRouter = require("./user");
+const accountRouter = require("./account");
+
+
+router.use("/user",userRouter);
+router.use("/account", accountRouter);
 
 module.exports = router;
 
